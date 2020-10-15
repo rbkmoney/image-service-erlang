b7873e38b777322bbb1ce5d73507c26e6280c144
Sergey Yelin <elinsn@gmail.com>
2020-10-15 16:22:39 +0300

    Use Erlang 22.3.4.11-alpine (#17)
    * Revert "Revert "Switch to mainstream Erlang image based on Alpine Linux (#15)" (#16)"
    
    This reverts commit 84467d3d0dd60740949730eee87a702e468e6ec6.
    
    * Use 22.3.4.11-alpine

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cat <<EOF
-FROM ${REGISTRY}/${ORG_NAME}/${BASE_IMAGE_NAME}:${BASE_IMAGE_TAG}
+FROM ${BASE_IMAGE}
 MAINTAINER Grigory Antsiferov <g.antsiferov@rbkmoney.com>
 LABEL com.rbkmoney.${SERVICE_NAME}.parent=${BASE_IMAGE_NAME}  \
-    com.rbkmoney.${SERVICE_NAME}.parent_tag=${BASE_IMAGE_TAG}  \
+    com.rbkmoney.${SERVICE_NAME}.parent_digest="${BASE_IMAGE_DIGEST}"  \
     com.rbkmoney.${SERVICE_NAME}.branch=${BRANCH}  \
     com.rbkmoney.${SERVICE_NAME}.commit_id=${COMMIT}  \
     com.rbkmoney.${SERVICE_NAME}.commit_number=`git rev-list --count HEAD`
