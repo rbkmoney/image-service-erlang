02a14b0cf68de5552e03a4f66f771411ff7964f8
Sergey Yelin <elinsn@gmail.com>
2020-10-27 18:26:29 +0300

    Add required curl (#18)
    * Add required curl
    
    * Make command multiline
    
    * Multiline fix (again)
    
    Co-authored-by: Toporkov Igor <i.toporkov@rbkmoney.com>

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -7,5 +7,11 @@ LABEL com.rbkmoney.${SERVICE_NAME}.parent=${BASE_IMAGE_NAME}  \
     com.rbkmoney.${SERVICE_NAME}.branch=${BRANCH}  \
     com.rbkmoney.${SERVICE_NAME}.commit_id=${COMMIT}  \
     com.rbkmoney.${SERVICE_NAME}.commit_number=`git rev-list --count HEAD`
+
+RUN set -xe \
+    && apk add --no-cache --virtual .run-deps curl \
+    && rm /var/cache/apk/*
+
+CMD ["sh"]
 EOF
 
