54a794b4875ad79f90dba0a7708190b3b37d584f
Sergey Yelin <elinsn@gmail.com>
2020-11-23 12:39:53 +0300

    Set default locale (#20)

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cat <<EOF
 FROM ${BASE_IMAGE}
+
+ENV LANG=C.UTF-8
+
 MAINTAINER Grigory Antsiferov <g.antsiferov@rbkmoney.com>
 LABEL com.rbkmoney.${SERVICE_NAME}.parent=${BASE_IMAGE_NAME}  \
     com.rbkmoney.${SERVICE_NAME}.parent_digest="${BASE_IMAGE_DIGEST}"  \
