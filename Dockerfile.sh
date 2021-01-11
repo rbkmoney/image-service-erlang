b2c2a3f8c46842488eef78a2574ba880eeff07b9
Sergey Yelin <elinsn@gmail.com>
2021-01-11 15:42:01 +0300

    Switch to using version tag (#21)
    * Switch to using version tag
    
    * Erlang 23.2.1

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -6,7 +6,7 @@ ENV LANG=C.UTF-8
 
 MAINTAINER Grigory Antsiferov <g.antsiferov@rbkmoney.com>
 LABEL com.rbkmoney.${SERVICE_NAME}.parent=${BASE_IMAGE_NAME}  \
-    com.rbkmoney.${SERVICE_NAME}.parent_digest="${BASE_IMAGE_DIGEST}"  \
+    com.rbkmoney.${SERVICE_NAME}.parent_tag="${BASE_IMAGE_TAG}"  \
     com.rbkmoney.${SERVICE_NAME}.branch=${BRANCH}  \
     com.rbkmoney.${SERVICE_NAME}.commit_id=${COMMIT}  \
     com.rbkmoney.${SERVICE_NAME}.commit_number=`git rev-list --count HEAD`
