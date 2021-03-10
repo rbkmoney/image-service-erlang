c0aee9a464ee26b8887dd9660dca69d4c3444179
Sergey Yelin <elinsn@gmail.com>
2021-03-10 13:16:11 +0300

    ED-75: Switch to debian (#27)
    * ED-75: Switch to debian
    
    * Update submodules
    
    * Cleanup

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -11,10 +11,18 @@ LABEL com.rbkmoney.${SERVICE_NAME}.parent=${BASE_IMAGE_NAME}  \
     com.rbkmoney.${SERVICE_NAME}.commit_id=${COMMIT}  \
     com.rbkmoney.${SERVICE_NAME}.commit_number=`git rev-list --count HEAD`
 
-RUN set -xe \
-    && apk add --no-cache --virtual .run-deps curl \
-    && rm /var/cache/apk/*
+ADD erlang.cfg /tmp
 
-CMD ["sh"]
+RUN set -xe \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends  curl equivs \
+    && cd /tmp \
+    && equivs-build erlang.cfg \
+    && dpkg -i erlang*.deb \
+    && apt-get purge -y --auto-remove equivs \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/erlang.cfg /tmp/erlang*.deb
+
+CMD ["bash"]
 EOF
 
--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 SERVICE_NAME := service-erlang
 
 BASE_IMAGE_NAME := library/erlang
-BASE_IMAGE_TAG := 23.2.5.0-alpine
+BASE_IMAGE_TAG := 23.2.7-slim
+ERLANG_VERSION := $(subst  -slim,,${BASE_IMAGE_TAG})
 
 UTILS_PATH := build_utils
 TEMPLATES_PATH := .
@@ -33,7 +34,11 @@ $(SUBTARGETS): %/.git: %
 
 submodules: $(SUBTARGETS)
 
-Dockerfile: Dockerfile.sh
+erlang.cfg: erlang.cfg.sh
+	ERLANG_VERSION=${ERLANG_VERSION} \
+	./erlang.cfg.sh > erlang.cfg
+
+Dockerfile: erlang.cfg Dockerfile.sh
 	REGISTRY=$(REGISTRY) ORG_NAME=$(ORG_NAME) \
 	BASE_IMAGE_NAME=$(BASE_IMAGE_NAME) BASE_IMAGE_TAG=$(BASE_IMAGE_TAG) \
 	BASE_IMAGE="$(BASE_IMAGE_NAME):$(BASE_IMAGE_TAG)" \
